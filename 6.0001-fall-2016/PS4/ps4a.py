b66fd1ea2df84d29a98bce7e04c046cfd9b17ebf
nicoataiza <christian.ataiza@obf.ateneo.edu>
2023-03-15 01:25:34 +0800

    solution for recursive formula

--- 6.0001-fall-2016/PS4/ps4a.py
+++ 6.0001-fall-2016/PS4/ps4a.py
@@ -3,6 +3,9 @@
 # Collaborators:
 # Time Spent: x:xx
 
+#from argon2 import DEFAULT_MEMORY_COST
+from itertools import permutations
+import copy
 def get_permutations(sequence):
     '''
     Enumerate all permutations of a given string
@@ -22,19 +25,32 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-
-    pass #delete this line and replace with your code here
+    ### Pseudo Code ###
+    # if length of sequence is 1; return output
+    if len(sequence) == 1:
+        return [sequence]
+    else:
+        first = sequence[0]
+        rest = get_permutations(sequence[1:])
+        out = []
+        for item in rest:
+            z = list(item)
+            for num in range(len(item)+1):
+                z.insert(num,first)
+                out.append("".join(z))
+                z.pop(num)
+    return out
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
+   #EXAMPLE
+   example_input = '123'
+   print('Input:', example_input)
+   print('Expected Output:', ["123","132","312","321","213","231"])
+   print('Actual Output:', get_permutations(example_input))
+   print("Checker:", set(["123","132","312","321","213","231"]) == set(get_permutations(example_input)))
+
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
 

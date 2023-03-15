eddb572912bbf5bd8c372cbeb4abb360f8657b6c
Nico Ataiza <christian.ataiza@paymaya.com>
2023-03-15 17:10:04 +0800

    ps4a, and ps4c

--- 6.0001-fall-2016/PS4/ps4a.py
+++ 6.0001-fall-2016/PS4/ps4a.py
@@ -22,19 +22,29 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-
-    pass #delete this line and replace with your code here
-
+    if len(sequence) == 1:
+        return [sequence]
+    else:
+        first = sequence[0]
+        rest = get_permutations(sequence[1:])
+        out = []
+        for item in rest:
+            z = list(item)
+            for num in range(len(item)+1):
+                z.insert(num,first)
+                out.append("".join(z))
+                z.pop(num)
+    return out
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
+   #EXAMPLE
+   example_input = 'abc'
+   print('Input:', example_input)
+   print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+   print('Actual Output:', get_permutations(example_input))
     
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+#     pass #delete this line and replace with your code here
 
